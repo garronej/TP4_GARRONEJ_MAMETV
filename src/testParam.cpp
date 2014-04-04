4025dcf6ca1cec76bd4ac58698d7139c88d1d67e
Joseph Garrone <jospeph.garrone@ensimag.imag.fr>
2014-04-04 05:14:47 +0200

    Il manque l'analise

---
+++ src/testParam.cpp
@@ -0,0 +1,39 @@
+/*!
+ * \file test.cpp
+ * \brief Fichier de tests unitaires.
+ * \author GARRONE Joseph, joseph.garrone@ensimag.grenoble-inp.fr
+ * \version 0.1 */
+#define EXIT_SUCCESS 0 
+#include <iostream> //std::cout, std::cin 
+#include <cstdlib>
+
+#include <vector>
+#include <list>
+
+#include "Point.h"
+#include "Triangle.h"
+#include "Maillage.h"
+
+using namespace std;
+
+int main ( int argc, char *argv[] ) {
+	cout	<< "\nProgram " << argv[0] << endl << endl;
+
+
+		int m = atoi(argv[1]);
+		int n = atoi(argv[2]);
+
+		cout<< "m = " << m << endl;
+		cout<< "n = " << n << endl;
+
+	if ( atoi(argv[3]) == 1){
+		cout << "list" << endl;
+		Maillage<float,list> M(Point<float>(2,3), Point<float>(5,6), Point<float>(3,8), Point<float>(0,5), m, n);
+	}else{
+		cout << "vector" << endl;
+		Maillage<float,vector> M(Point<float>(2,3), Point<float>(5,6), Point<float>(3,8), Point<float>(0,5), m, n);
+	}
+
+
+	return EXIT_SUCCESS;
+}
